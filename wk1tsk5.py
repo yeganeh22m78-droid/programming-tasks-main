5fa75061354cacc1ec84a55dcda804864e72c878
Yeganeh maleki <yeganeh22.m78@gmail.com>
2025-11-27 10:34:01 +0200

    Add files via upload

---
+++ wk1tsk5.py
@@ -0,0 +1,12 @@
+print("Calculate the area of a wall.")
+
+Feed = input("Enter the width in meters: ")
+Width = int(Feed)
+
+Feed = input("Enter the height in meters: ")
+Height = int(Feed)
+
+print(f"Width is {Width} m and height is {Height} m.")
+
+Area = Width * Height
+print(f"The wall will be {Area} square meters.")
